c1a4adeb6919e2fe5039db9f6810624e3751721f
sobebarali <sobebar.ali17@gmail.com>
2022-11-04 22:33:31 +0530

    ok

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('./db.json')
-const middlewares = jsonServer.defaults({
-  static: './build'
+const app = jsonServer.create()
+const path = require('path')
+const express = require('express')
+const cors = require("cors")
+const middlewares = jsonServer.defaults()
+const router = jsonServer.router('db.json')
+const port = process.env.PORT || 3004
+
+app.use('/db', middlewares, router,cors)
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-const PORT = 3004
-
-server.use(middlewares)
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-}))
-server.use(router)
-server.listen(PORT, () => {
-  console.log(`Server is running at ${PORT}`)
-})
+app.listen(port)
