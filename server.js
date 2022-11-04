6945b5063a976418548975df7e20a9722e51472a
sobebarali <sobebar.ali17@gmail.com>
2022-11-04 19:54:27 +0530

    make it betterffffffffffj

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const router = jsonServer.router('./db.json')
 const middlewares = jsonServer.defaults({
   static: './build'
 })
-const PORT = process.env.PORT || 3004
+
 server.use(middlewares)
 server.use(jsonServer.rewriter({
   '/api/*': '/$1',
 }))
 server.use(router)
-server.listen(PORT, () => {
+server.listen(3004, () => {
   console.log('Server is running')
 })
