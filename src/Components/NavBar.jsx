9de4f777f6a203d1e32be726f48af049655cbe53
sobebarali <sobebar.ali17@gmail.com>
2022-11-04 07:07:42 +0530

    Navbar

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,64 @@
+import React from "react"
+import { Flex, Spacer, Image, Text, Icon } from "@chakra-ui/react"
+import { Link } from "react-router-dom"
+import {BsSearch , BsBasket3} from 'react-icons/bs'
+import {BiUser} from 'react-icons/bi'
+
+const NavBar = () => {
+  return (
+    <Flex bg="white"
+    borderBottom="0.25px solid #b1b3b5"
+    p={1}
+    align="center"
+    justify="center"
+    >
+    <Spacer />
+      <Image
+        src="https://cdn.shopify.com/s/files/1/0258/2485/4100/files/flatheads-logo-new-hotizontal_180x_2x_bf74c8db-79f1-4904-b343-3b0e2681ec07_192x32.png?v=1647508945"
+        alt="Flathead Logo"
+        height="25px"
+        mt={15}
+        ml={20}
+      />
+      <Spacer />
+      <Link to="/collections/all">
+        <Text px={4} py={2}>
+          SHOP
+        </Text>
+      </Link>
+      <Link to="/collections/all">
+        <Text px={4} py={2}>
+          WOMEN
+        </Text>
+      </Link>
+      <Link to="/collections/all">
+        <Text px={4} py={2}>
+          MEN
+        </Text>
+      </Link>
+      <Link to="/collections/all">
+        <Text px={4} py={2}>
+          NEW ARRIVALS
+        </Text>
+      </Link>
+      <Link to="/collections/all">
+        <Text px={4} py={2}>
+          ABOUT
+        </Text>
+      </Link>
+      <Link to="/collections/all">
+        <Text px={4} py={2}>
+          HELP
+        </Text>
+      </Link>
+      <Spacer />
+      <Icon as={BsSearch} boxSize="20px" mx={6} />
+      <Icon as={BiUser} boxSize="20px" mx={6} />
+      <Icon as={BsBasket3} boxSize="20px" mx={6} />
+      <Text>0</Text>
+      <Spacer />
+    </Flex>
+  )
+}
+
+export default NavBar
